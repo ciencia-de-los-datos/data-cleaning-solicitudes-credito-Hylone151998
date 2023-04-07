331a54772ef4d8f56fb116644b77e4aa86d8dcc6
Hyrone15 <112992045+Hyrone15@users.noreply.github.com>
2023-04-07 10:25:11 -0500

    lab_5_intento_2

--- pregunta.py
+++ pregunta.py
@@ -11,37 +11,27 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col = 0)
-
-    #LOWER 
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
-    df['barrio'] = df['barrio'].str.lower()
-    df['idea_negocio'] = df['idea_negocio'].str.lower()
-    df['sexo'] = df['sexo'].str.lower()
-    df['línea_credito'] = df['línea_credito'].str.lower()
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
+    df.reset_index(inplace=True,drop=True)
     
-    # CAST
-    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
-    df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: x.replace('$', '')).apply(lambda x: x.replace(',', '')).astype(float).astype(int)
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True)
+    
+    df.dropna(axis='index',inplace=True)
+    df.drop_duplicates(inplace=True)
 
-    # DATE FORMAT
-    import datetime
-
-    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst= True)
-    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], format='%Y/%m/%d', infer_datetime_format = False ).dt.date
-
-    # CLEANING 
+    df['sexo'] = df['sexo'].str.lower().astype(str).str.strip()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower().astype(str)
+    df['idea_negocio'] = df['idea_negocio'].str.lower().astype(str)
+    df['barrio'] = df['barrio'].str.lower().astype(str)
+    df['línea_credito'] = df['línea_credito'].str.lower().astype(str)
+     
     df['idea_negocio'] = df['idea_negocio'].str.replace('_',' ').str.replace('-',' ').str.strip()
-    df['idea_negocio'] = df['idea_negocio'].apply(lambda x: x.replace(' ', '_'))
-
     df['barrio'] = df['barrio'].str.replace('_','-').str.replace('-',' ')
     df['línea_credito'] = df['línea_credito'].str.replace('_',' ').str.replace('-',' ').str.strip()
 
-    df = df.drop_duplicates()
-    df = df.dropna(subset=['tipo_de_emprendimiento'], axis=0)
-    df = df.dropna(subset=['idea_negocio'], axis=0)
-    df = df.dropna(subset=['barrio'], axis=0)
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',','').str.replace('$','',regex=False).str.replace(' ','').str.strip().astype(float)   
+    
+    df.drop_duplicates(inplace=True)
+    df.dropna(axis='index',inplace=True)
 
     return df
